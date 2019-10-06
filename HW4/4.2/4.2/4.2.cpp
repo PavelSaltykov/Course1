bdf4a80af9116c7e2a5c315654cada2b4f283bad
Pavel <pavsaltykov@mail.ru>
2019-10-06 14:09:53 +0300

    Вынес в отдельный модуль поиск наиболее часто встречающегося элемента Добавил комментарии к функциям

--- HW4/4.2/4.2/4.2.cpp
+++ HW4/4.2/4.2/4.2.cpp
@@ -3,31 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "qsort.h"
-
-int searchOfMinimumMostFrequent(int array[], int lengthOfArray)
-{
-	qsort(array, 0, lengthOfArray - 1);
-	int minimumMostFrequent = array[0];
-	int maxCounter = 1;
-	int tempCounter = 1;
-	for (int i = 1; i < lengthOfArray; i++)
-	{
-		if (array[i] == array[i - 1])
-		{
-			tempCounter++;
-		}
-		else
-		{
-			tempCounter = 1;
-		}
-		if (tempCounter > maxCounter)
-		{
-			maxCounter = tempCounter;
-			minimumMostFrequent = array[i];
-		}
-	}
-	return minimumMostFrequent;
-}
+#include "search.h"
 
 bool checkSortedArray(int array[], int lengthOfArray)
 {
