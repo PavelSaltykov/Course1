36d6e4117f69316243ee10277eb45ae9021d52a3
Pavel <pavsaltykov@mail.ru>
2019-10-08 01:32:23 +0300

    Изменил вывод чисел. Добавил условие на ввод, проверку введённого числа

--- HW4/4.1/4.1/4.1.cpp
+++ HW4/4.1/4.1/4.1.cpp
@@ -3,32 +3,65 @@
 
 void convert(char number, int binaryNumber[]) 
 {
-	printf("%d -> ", number);
 	int bit = 0b10000000;
 	for (int i = 0; i < 8; i++)
 	{
 		binaryNumber[i] = (number & bit) ? 1 : 0;
-		printf("%d", binaryNumber[i]);
 		bit = bit >> 1;
 	}
-	printf("\n");
+}
+
+bool rangeCheck(int number, int left, int right)
+{
+	return(number >= left && number <= right);
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	printf("Введите первое число: ");
+	int left = -128;
+	int right = 127;
+	printf("Введите первое целое число (из диапазона от %d до %d): ", left, right);
 	int temp;
 	scanf("%d", &temp);
+	if (temp >= 0 && rangeCheck(temp, left, right))
+	{
+		right -= temp;
+	}
+	else if(temp <= 0 && rangeCheck(temp, left, right))
+	{
+		left -= temp;
+	}
+	else
+	{
+		printf("Введённое число вне диапазона!");
+		return 1;
+	}
 	const char number1 = temp;
 	int binaryNumber1[8] = {};
 	convert(number1, binaryNumber1);
 
-	printf("Введите второе число: ");
+	printf("Введите второе целое число (из диапазона от %d до %d): ", left, right);
 	scanf("%d", &temp);
+	if (!rangeCheck(temp, left, right))
+	{
+		printf("Введённое число вне диапазона!");
+		return 1;
+	}
 	const char number2 = temp;
 	int binaryNumber2[8] = {};
 	convert(number2, binaryNumber2);
+
+	printf("%d -> ", number1);
+	for (int i = 0; i < 8; i++)
+	{
+		printf("%d", binaryNumber1[i]);
+	}
+	printf("\n%d -> ", number2);
+	for (int i = 0; i < 8; i++)
+	{
+		printf("%d", binaryNumber2[i]);
+	}
 	
 	return 0;
 }
