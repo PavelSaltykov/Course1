4b1edd2b29a1f5fd6bcf9105b223ef234ef7ed7b
Pavel <pavsaltykov@mail.ru>
2019-10-09 01:42:39 +0300

    Изменил перевод числа в двоичное и десятичное представление. Изменил тесты

--- HW4/4.1/4.1/4.1.cpp
+++ HW4/4.1/4.1/4.1.cpp
@@ -1,13 +1,13 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 
-void convertToBinary(char number, int binaryNumber[]) 
+void convertToBinary(int number, int binaryNumber[]) 
 {
-	int bit = 0b10000000;
-	for (int i = 0; i < 8; i++)
+	int bit = 1;
+	for (int i = 31; i >= 0; i--)
 	{
-		binaryNumber[i] = number & bit ? 1 : 0;
-		bit = bit >> 1;
+		binaryNumber[i] = (number & bit) ? 1 : 0;
+		bit = bit << 1;
 	}
 }
 
@@ -15,44 +15,27 @@ int convertToDecimal(int binaryNumber[])
 {
 	int decimalNumber = 0;
 	int powerOf2 = 1;
-	bool isNegative = false;
-	if (binaryNumber[0] == 1)
+	for (int i = 31; i >= 0; i--)
 	{
-		isNegative = true;
-	}
-	for (int i = 7; i >= 0; i--)
-	{
-		if (isNegative)
-		{
-			binaryNumber[i] = binaryNumber[i] == 1 ? 0 : 1;
-		}
 		decimalNumber += powerOf2 * binaryNumber[i];
 		powerOf2 *= 2;
 	}
-	return isNegative ? -decimalNumber - 1 : decimalNumber;
-}
-
-bool rangeCheck(int number, int left, int right)
-{
-	return number >= left && number <= right;
+	return decimalNumber;
 }
 
 void sumOfTwoBinaryNumbers(int firstNumber[], int secondNumber[], int sumOfNumbers[])
 {
 	int carryOver = 0;
-	for (int i = 7; i >= 0; i--)
+	for (int i = 31; i >= 0; i--)
 	{
 		sumOfNumbers[i] = (carryOver + firstNumber[i] + secondNumber[i]) % 2;
-		if (i != 0)
-		{
-			carryOver = (carryOver + firstNumber[i] + secondNumber[i]) / 2;
-		}
+		carryOver = (carryOver + firstNumber[i] + secondNumber[i]) / 2;
 	}
 }
 
 bool checkBinaryNumber(int binaryNumber[], int answer[])
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 32; i++)
 	{
 		if (binaryNumber[i] != answer[i])
 		{
@@ -62,31 +45,42 @@ bool checkBinaryNumber(int binaryNumber[], int answer[])
 	return true;
 }
 
+void printOfBinaryNumber(int array[])
+{
+	for (int i = 0; i < 32; i++)
+	{
+		if (i % 8 == 0)
+		{
+			printf(" ");
+		}
+		printf("%d", array[i]);
+	}
+}
+
 bool testConvertToBinary()
 {
-	int number = 5;
-	int binaryNumber5[8] = {};
-	convertToBinary(number, binaryNumber5);
-	int answer5[8] = {0, 0, 0, 0, 0, 1, 0, 1};
-	number = 127;
-	int binaryNumber127[8] = {};
-	convertToBinary(number, binaryNumber127);
-	int answer127[8] = {0, 1, 1, 1, 1, 1, 1, 1};
-	if (!checkBinaryNumber(binaryNumber5, answer5) && !checkBinaryNumber(binaryNumber127, answer127))
+	int number = 515;
+	int binaryNumber515[32] = {};
+	convertToBinary(number, binaryNumber515);
+	int answer515[32] = {};
+	answer515[22] = 1;
+	answer515[30] = 1;
+	answer515[31] = 1;
+	if (!checkBinaryNumber(binaryNumber515, answer515))
 	{
 		printf("Ошибка при переводе положительного числа в двоичное представление\n");
 		return false;
 	}
 
-	number = -1;
-	int binaryNumber1[8] = {};
-	convertToBinary(number, binaryNumber1);
-	int answer1[8] = {1, 1, 1, 1, 1, 1, 1, 1};
-	number = -128;
-	int binaryNumber128[8] = {};
-	convertToBinary(number, binaryNumber128);
-	int answer128[8] = {1, 0, 0, 0, 0, 0, 0, 0};
-	if (!checkBinaryNumber(binaryNumber1, answer1) && !checkBinaryNumber(binaryNumber128, answer128))
+	number = -2057;
+	int binaryNumber2057[32] = {};
+	convertToBinary(number, binaryNumber2057);
+	int answer2057[32] = {};
+	for (int i = 0; i < 32; i++)
+	{
+		answer2057[i] = (i == 20 || i == 28) ? 0 : 1;
+	}
+	if (!checkBinaryNumber(binaryNumber2057, answer2057))
 	{
 		printf("Ошибка при переводе отрицательного числа в двоичное представление\n");
 		return false;
@@ -96,25 +90,29 @@ bool testConvertToBinary()
 
 bool checkSum()
 {
-	int binaryNumber11[8] = {0, 0, 0, 0, 1, 0, 1, 1};
-	int binaryNumber7[8] = {0, 0, 0, 0, 0, 1, 1, 1};
-	int binarySum18[8] = {};
-	sumOfTwoBinaryNumbers(binaryNumber11, binaryNumber7, binarySum18);
-	int answer18[8] = {0, 0, 0, 1, 0, 0, 1, 0};
-	if (!checkBinaryNumber(binarySum18, answer18))
+	int binaryNumber4600[32] = {};
+	binaryNumber4600[19] = 1;
+	for (int i = 23; i < 29; i++)
 	{
-		printf("Ошибка при сложении чисел\n");
-		return false;
+		binaryNumber4600[i] = 1;
+	}
+	int binaryNumber5688[32] = {};
+	for (int i = 0; i < 29; i++)
+	{
+		binaryNumber5688[i] = (i == 19 || i == 21 || i == 22 || i == 26 || i == 27) ? 0 : 1;
 	}
 
-	int binaryNumber15[8] = {1, 1, 1, 1, 0, 0, 0, 1};
-	int binaryNumber58[8] = {1, 1, 0, 0, 0, 1, 1, 0};
-	int binarySum73[8] = {};
-	sumOfTwoBinaryNumbers(binaryNumber15, binaryNumber58, binarySum73);
-	int answer73[8] = {1, 0, 1, 1, 0, 1, 1, 1};
-	if (!checkBinaryNumber(binarySum73, answer73))
+	int binarySum1088[32] = {};
+	sumOfTwoBinaryNumbers(binaryNumber4600, binaryNumber5688, binarySum1088);
+	int answer1088[32] = {};
+	for (int i = 0; i < 26; i++)
 	{
-		printf("Ошибка при отрицательной сумме\n");
+		answer1088[i] = (i == 21) ? 0 : 1;
+	}
+
+	if (!checkBinaryNumber(binarySum1088, answer1088))
+	{
+		printf("Ошибка при сложении чисел\n");
 		return false;
 	}
 	return true;
@@ -122,15 +120,24 @@ bool checkSum()
 
 bool checkConvertToDecimal()
 {
-	int binaryNumber63[8] = {0, 0, 1, 1, 1, 1, 1, 1};
-	if (convertToDecimal(binaryNumber63) != 63)
+	int binaryNumber4600[32] = {};
+	binaryNumber4600[19] = 1;
+	for (int i = 23; i < 29; i++)
+	{
+		binaryNumber4600[i] = 1;
+	}
+	if (convertToDecimal(binaryNumber4600) != 4600)
 	{
 		printf("Ошибка при переводе положительного числа в десятичное представление\n");
 		return false;
 	}
 
-	int binaryNumber85[8] = {1, 0, 1, 0, 1, 0, 1, 1};
-	if (convertToDecimal(binaryNumber85) != -85)
+	int binaryNumber1088[32] = {};
+	for (int i = 0; i < 26; i++)
+	{
+		binaryNumber1088[i] = (i == 21) ? 0 : 1;
+	}
+	if (convertToDecimal(binaryNumber1088) != -1088)
 	{
 		printf("Ошибка при переводе отрицательного числа в десятичное представление\n");
 		return false;
@@ -150,58 +157,29 @@ int main()
 	{
 		return 1;
 	}
-	int left = -128;
-	int right = 127;
-	printf("Введите первое целое число (из диапазона от %d до %d): ", left, right);
-	int temp;
-	scanf("%d", &temp);
-	if (temp >= 0 && rangeCheck(temp, left, right))
-	{
-		right -= temp;
-	}
-	else if(temp <= 0 && rangeCheck(temp, left, right))
-	{
-		left -= temp;
-	}
-	else
-	{
-		printf("Введённое число вне диапазона!");
-		return 1;
-	}
-	const char number1 = temp;
-	int binaryNumber1[8] = {};
+	printf("Введите первое целое число: ");
+	int number1 = 0;
+	scanf("%d", &number1);
+	int binaryNumber1[32] = {};
 	convertToBinary(number1, binaryNumber1);
 
-	printf("Введите второе целое число (из диапазона от %d до %d): ", left, right);
-	scanf("%d", &temp);
-	if (!rangeCheck(temp, left, right))
-	{
-		printf("Введённое число вне диапазона!");
-		return 1;
-	}
-	const char number2 = temp;
-	int binaryNumber2[8] = {};
+	printf("Введите второе целое число: ");
+	int number2 = 0;
+	scanf("%d", &number2);
+	int binaryNumber2[32] = {};
 	convertToBinary(number2, binaryNumber2);
 
 	printf("\nДвоичное представление в дополнительном коде:\n");
-	printf("%d -> ", number1);
-	for (int i = 0; i < 8; i++)
-	{
-		printf("%d", binaryNumber1[i]);
-	}
-	printf("\n%d -> ", number2);
-	for (int i = 0; i < 8; i++)
-	{
-		printf("%d", binaryNumber2[i]);
-	}
-	
-	printf("\n\nСумма этих чисел: ");
-	int binarySum[8] = {};
+	printf("%d ->", number1);
+	printOfBinaryNumber(binaryNumber1);
+	printf("\n%d ->", number2);
+	printOfBinaryNumber(binaryNumber2);
+
+	printf("\n\nСумма:");
+	int binarySum[32] = {};
 	sumOfTwoBinaryNumbers(binaryNumber1, binaryNumber2, binarySum);
-	for (int i = 0; i < 8; i++)
-	{
-		printf("%d", binarySum[i]);
-	}
+	printOfBinaryNumber(binarySum);
+
 	printf(" -> %d\n", convertToDecimal(binarySum));
 	return 0;
 }
