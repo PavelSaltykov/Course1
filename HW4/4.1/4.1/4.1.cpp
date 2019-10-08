51b08c7f24d53fb269097abcfa0bd9ad6aeed698
Pavel <pavsaltykov@mail.ru>
2019-10-08 07:53:05 +0300

    Добавил тесты

--- HW4/4.1/4.1/4.1.cpp
+++ HW4/4.1/4.1/4.1.cpp
@@ -50,9 +50,106 @@ void sumOfTwoBinaryNumbers(int firstNumber[], int secondNumber[], int sumOfNumbe
 	}
 }
 
+bool checkBinaryNumber(int binaryNumber[], int answer[])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		if (binaryNumber[i] != answer[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool testConvertToBinary()
+{
+	int number = 5;
+	int binaryNumber5[8] = {};
+	convertToBinary(number, binaryNumber5);
+	int answer5[8] = {0, 0, 0, 0, 0, 1, 0, 1};
+	number = 127;
+	int binaryNumber127[8] = {};
+	convertToBinary(number, binaryNumber127);
+	int answer127[8] = {0, 1, 1, 1, 1, 1, 1, 1};
+	if (!checkBinaryNumber(binaryNumber5, answer5) && !checkBinaryNumber(binaryNumber127, answer127))
+	{
+		printf("Ошибка при переводе положительного числа в двоичное представление\n");
+		return false;
+	}
+
+	number = -1;
+	int binaryNumber1[8] = {};
+	convertToBinary(number, binaryNumber1);
+	int answer1[8] = {1, 1, 1, 1, 1, 1, 1, 1};
+	number = -128;
+	int binaryNumber128[8] = {};
+	convertToBinary(number, binaryNumber128);
+	int answer128[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+	if (!checkBinaryNumber(binaryNumber1, answer1) && !checkBinaryNumber(binaryNumber128, answer128))
+	{
+		printf("Ошибка при переводе отрицательного числа в двоичное представление\n");
+		return false;
+	}
+	return true;
+}
+
+bool checkSum()
+{
+	int binaryNumber11[8] = {0, 0, 0, 0, 1, 0, 1, 1};
+	int binaryNumber7[8] = {0, 0, 0, 0, 0, 1, 1, 1};
+	int binarySum18[8] = {};
+	sumOfTwoBinaryNumbers(binaryNumber11, binaryNumber7, binarySum18);
+	int answer18[8] = {0, 0, 0, 1, 0, 0, 1, 0};
+	if (!checkBinaryNumber(binarySum18, answer18))
+	{
+		printf("Ошибка при сложении чисел\n");
+		return false;
+	}
+
+	int binaryNumber15[8] = {1, 1, 1, 1, 0, 0, 0, 1};
+	int binaryNumber58[8] = {1, 1, 0, 0, 0, 1, 1, 0};
+	int binarySum73[8] = {};
+	sumOfTwoBinaryNumbers(binaryNumber15, binaryNumber58, binarySum73);
+	int answer73[8] = {1, 0, 1, 1, 0, 1, 1, 1};
+	if (!checkBinaryNumber(binarySum73, answer73))
+	{
+		printf("Ошибка при отрицательной сумме\n");
+		return false;
+	}
+	return true;
+}
+
+bool checkConvertToDecimal()
+{
+	int binaryNumber63[8] = {0, 0, 1, 1, 1, 1, 1, 1};
+	if (convertToDecimal(binaryNumber63) != 63)
+	{
+		printf("Ошибка при переводе положительного числа в десятичное представление\n");
+		return false;
+	}
+
+	int binaryNumber85[8] = {1, 0, 1, 0, 1, 0, 1, 1};
+	if (convertToDecimal(binaryNumber85) != -85)
+	{
+		printf("Ошибка при переводе отрицательного числа в десятичное представление\n");
+		return false;
+	}
+	return true;
+}
+
+bool tests()
+{
+	return testConvertToBinary() && checkSum() && checkConvertToDecimal();
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	if (!tests())
+	{
+		return 1;
+	}
 	int left = -128;
 	int right = 127;
 	printf("Введите первое целое число (из диапазона от %d до %d): ", left, right);
