f69fbb6a4e04493c9beb2ca22160f8ffabca7c50
Pavel <pavsaltykov@mail.ru>
2019-10-09 21:22:53 +0300

    Добавил глобальную константу

--- HW4/4.1/4.1/4.1.cpp
+++ HW4/4.1/4.1/4.1.cpp
@@ -1,10 +1,12 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 
+const int size = sizeof(int) * 8;
+
 void convertToBinary(int number, int binaryNumber[]) 
 {
 	int bit = 1;
-	for (int i = 31; i >= 0; i--)
+	for (int i = size - 1; i >= 0; i--)
 	{
 		binaryNumber[i] = (number & bit) ? 1 : 0;
 		bit = bit << 1;
@@ -15,7 +17,7 @@ int convertToDecimal(int binaryNumber[])
 {
 	int decimalNumber = 0;
 	int powerOf2 = 1;
-	for (int i = 31; i >= 0; i--)
+	for (int i = size - 1; i >= 0; i--)
 	{
 		decimalNumber += powerOf2 * binaryNumber[i];
 		powerOf2 *= 2;
@@ -26,7 +28,7 @@ int convertToDecimal(int binaryNumber[])
 void sumOfTwoBinaryNumbers(int firstNumber[], int secondNumber[], int sumOfNumbers[])
 {
 	int carryOver = 0;
-	for (int i = 31; i >= 0; i--)
+	for (int i = size - 1; i >= 0; i--)
 	{
 		sumOfNumbers[i] = (carryOver + firstNumber[i] + secondNumber[i]) % 2;
 		carryOver = (carryOver + firstNumber[i] + secondNumber[i]) / 2;
@@ -35,7 +37,7 @@ void sumOfTwoBinaryNumbers(int firstNumber[], int secondNumber[], int sumOfNumbe
 
 bool checkBinaryNumber(int binaryNumber[], int answer[])
 {
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (binaryNumber[i] != answer[i])
 		{
@@ -47,7 +49,7 @@ bool checkBinaryNumber(int binaryNumber[], int answer[])
 
 void printOfBinaryNumber(int array[])
 {
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (i % 8 == 0)
 		{
@@ -60,9 +62,9 @@ void printOfBinaryNumber(int array[])
 bool testConvertToBinary()
 {
 	int number = 515;
-	int binaryNumber515[32] = {};
+	int binaryNumber515[size] = {};
 	convertToBinary(number, binaryNumber515);
-	int answer515[32] = {};
+	int answer515[size] = {};
 	answer515[22] = 1;
 	answer515[30] = 1;
 	answer515[31] = 1;
@@ -73,10 +75,10 @@ bool testConvertToBinary()
 	}
 
 	number = -2057;
-	int binaryNumber2057[32] = {};
+	int binaryNumber2057[size] = {};
 	convertToBinary(number, binaryNumber2057);
-	int answer2057[32] = {};
-	for (int i = 0; i < 32; i++)
+	int answer2057[size] = {};
+	for (int i = 0; i < size; i++)
 	{
 		answer2057[i] = (i == 20 || i == 28) ? 0 : 1;
 	}
@@ -90,21 +92,21 @@ bool testConvertToBinary()
 
 bool checkSum()
 {
-	int binaryNumber4600[32] = {};
+	int binaryNumber4600[size] = {};
 	binaryNumber4600[19] = 1;
 	for (int i = 23; i < 29; i++)
 	{
 		binaryNumber4600[i] = 1;
 	}
-	int binaryNumber5688[32] = {};
+	int binaryNumber5688[size] = {};
 	for (int i = 0; i < 29; i++)
 	{
 		binaryNumber5688[i] = (i == 19 || i == 21 || i == 22 || i == 26 || i == 27) ? 0 : 1;
 	}
 
-	int binarySum1088[32] = {};
+	int binarySum1088[size] = {};
 	sumOfTwoBinaryNumbers(binaryNumber4600, binaryNumber5688, binarySum1088);
-	int answer1088[32] = {};
+	int answer1088[size] = {};
 	for (int i = 0; i < 26; i++)
 	{
 		answer1088[i] = (i == 21) ? 0 : 1;
@@ -120,7 +122,7 @@ bool checkSum()
 
 bool checkConvertToDecimal()
 {
-	int binaryNumber4600[32] = {};
+	int binaryNumber4600[size] = {};
 	binaryNumber4600[19] = 1;
 	for (int i = 23; i < 29; i++)
 	{
@@ -132,7 +134,7 @@ bool checkConvertToDecimal()
 		return false;
 	}
 
-	int binaryNumber1088[32] = {};
+	int binaryNumber1088[size] = {};
 	for (int i = 0; i < 26; i++)
 	{
 		binaryNumber1088[i] = (i == 21) ? 0 : 1;
@@ -160,13 +162,13 @@ int main()
 	printf("Введите первое целое число: ");
 	int number1 = 0;
 	scanf("%d", &number1);
-	int binaryNumber1[32] = {};
+	int binaryNumber1[size] = {};
 	convertToBinary(number1, binaryNumber1);
 
 	printf("Введите второе целое число: ");
 	int number2 = 0;
 	scanf("%d", &number2);
-	int binaryNumber2[32] = {};
+	int binaryNumber2[size] = {};
 	convertToBinary(number2, binaryNumber2);
 
 	printf("\nДвоичное представление в дополнительном коде:\n");
@@ -176,7 +178,7 @@ int main()
 	printOfBinaryNumber(binaryNumber2);
 
 	printf("\n\nСумма:");
-	int binarySum[32] = {};
+	int binarySum[size] = {};
 	sumOfTwoBinaryNumbers(binaryNumber1, binaryNumber2, binarySum);
 	printOfBinaryNumber(binarySum);
 
