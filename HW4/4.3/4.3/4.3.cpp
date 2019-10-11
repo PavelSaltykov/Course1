6d0619bc85b867ae0694403ba9f698f4da8c5d33
Pavel <pavsaltykov@mail.ru>
2019-10-11 21:52:06 +0300

    first commit

---
+++ HW4/4.3/4.3/4.3.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+
+void begin()
+{
+	printf("\nВведите:\n 0 - выйти\n");
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	printf("Телефонный справочник\n");
+	char input[100] = "";
+	bool isExit = false;
+	while (!isExit)
+	{
+		begin();
+		scanf("%s", input);
+		if (strlen(input) != 1)
+		{
+			printf("Неверный ввод, попробуйте ещё раз.\n");
+			continue;
+		}
+		switch (input[0])
+		{
+		case '0':
+			isExit = true;
+			break;
+		default:
+			printf("Неверный ввод, попробуйте ещё раз.\n");
+			break;
+		}
+	}
+	return 0;
+}
