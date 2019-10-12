4408f4b77148783ab6e5bad02cc1fd17eb52c84c
Pavel <pavsaltykov@mail.ru>
2019-10-12 21:07:16 +0300

    Добавил операции 1 и 5 (новая запись и сохранение данных в файл)

--- HW4/4.3/4.3/4.3.cpp
+++ HW4/4.3/4.3/4.3.cpp
@@ -1,19 +1,87 @@
 ﻿#include <stdio.h>
-#include <locale.h>
 #include <windows.h>
 
 void begin()
 {
 	printf("\nВведите:\n 0 - выйти\n");
+	printf(" 1 - добавить запись (имя и телефон)\n");
+	printf(" 5 - сохранить текущие данные\n");
+}
+
+int countEntriesInBase()
+{
+	FILE *file = fopen("database.txt", "r");
+	int countEntries = 0;
+	if (!file)
+	{
+		return countEntries;
+	}
+	while (!feof(file))
+	{
+		if (fgetc(file) == '\n')
+		{
+			countEntries++;
+		}
+	}
+	fclose(file);
+	return countEntries;
+}
+
+const int baseSize = 100;
+const int entryLength = 100;
+
+void addEntry(char (&entries)[baseSize][entryLength], int &amountOfEntriesInBuffer)
+{
+	if (countEntriesInBase() + amountOfEntriesInBuffer < 100)
+	{
+		printf("Введите имя: ");
+		char name[50] = {};
+		scanf("%s", name);
+		printf("Введите номер телефона: ");
+		char phoneNumber[50] = {};
+		scanf("%s", phoneNumber);
+
+		strncat(entries[amountOfEntriesInBuffer], name, strlen(name));
+		strcat(entries[amountOfEntriesInBuffer], " ");
+		strncat(entries[amountOfEntriesInBuffer], phoneNumber, strlen(phoneNumber));
+		amountOfEntriesInBuffer++;
+	}
+	else
+	{
+		printf("В базе данных закончилось место\n");
+	}
+}
+
+void saveData(char (&entries)[100][100], int &amountOfEntriesInBuffer)
+{
+	if (amountOfEntriesInBuffer == 0)
+	{
+		printf("Новых записей нет\n");
+	}
+	else
+	{
+		FILE *file = fopen("database.txt", "a");
+		for (int i = 0; i < amountOfEntriesInBuffer; ++i)
+		{
+			fprintf(file, "%s\n", entries[i]);
+		}
+		fclose(file);
+		printf("Данные сохранены\n");
+		amountOfEntriesInBuffer = 0;
+	}
 }
 
 int main()
 {
-	setlocale(LC_ALL, "Rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
 	printf("Телефонный справочник\n");
+	int amountOfEntriesInBuffer = 0;
+	char entriesInBuffer[baseSize][entryLength] = {};
 	char input[100] = "";
-	bool isExit = false;
-	while (!isExit)
+	bool shouldGoOut = false;
+
+	while (!shouldGoOut)
 	{
 		begin();
 		scanf("%s", input);
@@ -25,7 +93,13 @@ int main()
 		switch (input[0])
 		{
 		case '0':
-			isExit = true;
+			shouldGoOut = true;
+			break;
+		case '1':
+			addEntry(entriesInBuffer, amountOfEntriesInBuffer);
+			break;
+		case '5':
+			saveData(entriesInBuffer, amountOfEntriesInBuffer);
 			break;
 		default:
 			printf("Неверный ввод, попробуйте ещё раз.\n");
