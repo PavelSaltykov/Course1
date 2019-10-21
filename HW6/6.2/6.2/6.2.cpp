073c6575307c1f77b42cff5ccbb6aa6bafe74291
Pavel <pavsaltykov@mail.ru>
2019-10-21 06:47:16 +0300

    Изменил тесты

--- HW6/6.2/6.2/6.2.cpp
+++ HW6/6.2/6.2/6.2.cpp
@@ -18,7 +18,7 @@ char openBracket(char closedBracket)
 	}
 }
 
-bool checkString(char string[], StackElement *top)
+bool checkString(char string[], StackElement **top)
 {
 	const int length = strlen(string);
 	for (int i = 0; i < length; i++)
@@ -28,50 +28,59 @@ bool checkString(char string[], StackElement *top)
 		case '(':
 		case '{':
 		case '[':
-			push(&top, string[i]);
+			push(top, string[i]);
 			break;
 
 		case ')':
 		case '}':
 		case ']':
-			if (isEmpty(top))
+			if (isEmpty(*top))
 			{
 				return false;
 			}
-			else if (pop(&top) != openBracket(string[i]))
+			else if (pop(top) != openBracket(string[i]))
 			{
 				return false;
 			}
 			break;
 		}
 	}
-	return isEmpty(top);
+	return isEmpty(*top);
 }
 
-bool tests(StackElement *top)
+bool tests()
 {
+	StackElement *topForTest1 = nullptr;
 	char testString1[5] = "({)}";
+	const bool test1Passed = !checkString(testString1, &topForTest1);
+	delete topForTest1;
+
+	StackElement *topForTest2 = nullptr;
 	char testString2[9] = "({}[()])";
+	const bool test2Passed = checkString(testString2, &topForTest2);
+	delete topForTest2;
+
+	StackElement *topForTest3 = nullptr;
 	char testString3[24] = "string without brackets";
-	return !checkString(testString1, top) && checkString(testString2, top) && checkString(testString3, top);
+	const bool test3Passed = checkString(testString3, &topForTest3);
+	delete topForTest3;
+		
+	return test1Passed && test2Passed && test3Passed;
 }
 
 int main()
 {
-	StackElement *topForTests = nullptr;
-	if (!tests(topForTests))
+	if (!tests())
 	{
 		printf("Testing error\n");
-		delete topForTests;
 		return 1;
 	}
-	delete topForTests;
 
 	StackElement *top = nullptr;
 	printf("Enter the string: ");
 	char string[1000] = {};
 	fgets(string, 1000, stdin);
-	printf(checkString(string, top) ? "balanced" : "unbalanced");
+	printf(checkString(string, &top) ? "balanced" : "unbalanced");
 	delete top;
 	return 0;
 }
