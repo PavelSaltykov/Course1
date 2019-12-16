5b95d908e270d4bff7b24804ac8d9fbe24a6372f
Pavel <pavsaltykov@mail.ru>
2019-12-17 01:11:34 +0300

    Добавил чтение из файла

--- HW8/8.2/8.2/8.2.cpp
+++ HW8/8.2/8.2/8.2.cpp
@@ -1,12 +1,36 @@
 ﻿#include <stdio.h>
 #include "tree.h"
 
+bool tests()
+{
+
+	return true;
+}
+
 int main()
 {
-	char string[] = "* 34 - 56 110";
-	Tree *tree = build(string);
+	if (!tests())
+	{
+		printf("Testing error\n");
+		return 1;
+	}
+
+	FILE *file = fopen("input.txt", "r");
+	if (!file)
+	{
+		printf("File not found");
+		return 1;
+	}
+
+	char prefix[1000] = {};
+	fgets(prefix, 1000, file);
+	fclose(file);
+
+	Tree *tree = build(prefix);
+	printf("Prefix expression: ");
 	printTree(tree);
-	printf("\n%d", calculate(tree));
+	printf("\nReult: %d", calculate(tree));
+
 	deleteTree(tree);
 	return 0;
 }
