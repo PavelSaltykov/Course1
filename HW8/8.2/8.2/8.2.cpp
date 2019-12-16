12d04187f881cb173fdde713f81448532c3b1a45
Pavel <pavsaltykov@mail.ru>
2019-12-17 01:57:20 +0300

    Добавил тесты

--- HW8/8.2/8.2/8.2.cpp
+++ HW8/8.2/8.2/8.2.cpp
@@ -1,10 +1,23 @@
 ﻿#include <stdio.h>
 #include "tree.h"
 
+bool test(char *string, int answer)
+{
+	Tree *tree = build(string);
+	const bool testPassed = calculate(tree) == answer;
+	deleteTree(tree);
+	return testPassed;
+}
+
 bool tests()
 {
+	char string1[] = "* (+ 52 71) (- 6 31)";
+	const int answer1 = -3075;
 
-	return true;
+	char string2[] = "(- (* 25 (- 70 35))(/ (* (+ 15 110) 6) 7))";
+	const int answer2 = 768;
+
+	return test(string1, answer1) && test(string2, answer2);
 }
 
 int main()
