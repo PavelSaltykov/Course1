d69eb6e0ed82ba1e19b03555617939f489afc95f
Pavel <pavsaltykov@mail.ru>
2019-12-17 14:51:55 +0300

    Тест 3.1

---
+++ Test3/test3.1/test3.1/test3.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include "list.h"
+
+int main()
+{
+	printf("Entrer numbers: ");
+	List *list = createList();
+	while (true)
+	{
+		int number = 0;
+		scanf("%d", &number);
+		if (number == 0)
+		{
+			break;
+		}
+		addValueToSortedList(list, number);
+	}
+	printf("Numbers without repetition in ascending order: ");
+	printList(list);
+	deleteList(list);
+	return 0;
+}
