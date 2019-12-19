2d8f3e67a7f0014c4b2b67bd47901972da11d52f
Pavel <pavsaltykov@mail.ru>
2019-12-20 02:42:54 +0300

    Добавил комментарии и чтение из файла

--- HW10/10.1/10.1/10.1.cpp
+++ HW10/10.1/10.1/10.1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "hashTable.h"
 
 bool tests()
@@ -7,7 +8,7 @@ bool tests()
 	HashTable *table = createHashTable();
 	char string1[1000] = "a";
 	char string2[2] = "M";
-	char string3[2] = "b";
+	char string3[5] = "bcdf";
 	for (int i = 0; i < 900; ++i)
 	{
 		strcat(string1, "a");
@@ -15,14 +16,21 @@ bool tests()
 		addValueToTable(table, string2);
 		addValueToTable(table, string3);
 	}
-	const bool testsPassed = tableContainsValue(table, string3);
+	bool testsPassed = tableContainsValue(table, string1);
+	testsPassed = testsPassed && tableContainsValue(table, string2);
+	testsPassed = testsPassed && tableContainsValue(table, string3);
 
-	printTable(table);
-	printf("\n%f", loadFactor(table));
-	printf("\n%d", maximumListLength(table));
-	printf("\n%f", averageListLength(table));
 	deleteTable(table);
 	return testsPassed;
+}
+
+void parser(char *string)
+{
+	int last = strlen(string) - 1;
+	if (!isalnum(string[last]))
+	{
+		string[last] = '\0';
+	}
 }
 
 int main()
@@ -33,6 +41,33 @@ int main()
 		return 1;
 	}
 
+	FILE *file = fopen("input.txt", "r");
+	if (!file)
+	{
+		printf("File not found");
+		return 1;
+	}
 
+	HashTable *table = createHashTable();
+	while (!feof(file))
+	{
+		char word[50] = {};
+		fscanf(file, "%s", word);
+		parser(word);
+		if (strlen(word) > 0)
+		{
+			addValueToTable(table, word);
+		}
+	}
+	fclose(file);
+
+	printf("Words:\n");
+	printTable(table);
+
+	printf("\n\nLoad factor: %f", loadFactor(table));
+	printf("\nMaximum list length: %d", maximumListLength(table));
+	printf("\nAverage list length: %f\n", averageListLength(table));
+
+	deleteTable(table);
 	return 0;
 }
