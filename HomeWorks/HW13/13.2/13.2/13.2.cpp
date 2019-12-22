8a31e4f492b58f74963d4ddcb04538813d434742
Pavel <pavsaltykov@mail.ru>
2019-12-22 18:08:48 +0300

    Добавил 13.2

--- HomeWorks/HW13/13.2/13.2/13.2.cpp
+++ HomeWorks/HW13/13.2/13.2/13.2.cpp
@@ -1,7 +1,23 @@
 ﻿#include <stdio.h>
+#include <string.h>
+#include "searchComments.h"
+
+bool tests()
+{
+	char fileName[] = "test.txt";
+	char answer[] = "/*ads/*dsf***/\n/*qwe/*/\n";
+	return strcmp(comments(fileName), answer) == 0;
+}
 
 int main()
 {
+	if (!tests())
+	{
+		printf("Testing error\n");
+		return 1;
+	}
 
+	char fileName[] = "input.txt";
+	printf("Comments from file:\n%s", comments(fileName));
 	return 0;
 }
