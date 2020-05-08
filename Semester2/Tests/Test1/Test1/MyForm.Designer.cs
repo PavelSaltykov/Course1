c455fea34e97c21b2549df7cdceddfce44a3079a
Pavel <pavsaltykov@mail.ru>
2020-05-08 17:00:38 +0300

    added test1

---
+++ Semester2/Tests/Test1/Test1/MyForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Test1
+{
+    partial class MyForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(314, 186);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(140, 50);
+            this.button.TabIndex = 0;
+            this.button.Text = "Click to close";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.ButtonClick);
+            this.button.MouseHover += new System.EventHandler(this.ButtonMouseHover);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Name = "MyForm";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+    }
+}
+
