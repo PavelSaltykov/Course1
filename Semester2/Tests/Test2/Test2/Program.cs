ac867947cdd246c6795058ef8db606a6d20da272
Pavel <pavsaltykov@mail.ru>
2020-05-29 16:59:46 +0300

    added test2

---
+++ Semester2/Tests/Test2/Test2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Test2
+{
+    public class Program
+    {
+        /// <summary>
+        /// Generic method bubble sort.
+        /// </summary>
+        /// <typeparam name="T">Type of list items"</typeparam>
+        /// <param name="list">List to sort</param>
+        /// <param name="comparer">Object that allows to compare list items</param>
+        public static void BubbleSort<T>(IList<T> list, IComparer<T> comparer)
+        {
+            for (var i = 0; i < list.Count; ++i)
+            {
+                for (var j = list.Count - 1; j > i; --j)
+                {
+                    if (comparer.Compare(list[j], list[j - 1]) < 0)
+                    {
+                        (list[j], list[j - 1]) = (list[j - 1], list[j]);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Semester2/Tests/Test2/Test2Tests/ProgramTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Test2.Tests
+{
+    [TestFixture()]
+    public class ProgramTests
+    {
+        private readonly Comparer<int> comparer = Comparer<int>.Default;
+
+        static readonly object[] Cases =
+        {
+            new object[] { new List<int>(), new List<int>() },
+            new object[] { new List<int> { -1, 2, 3, 1, 0 }, new List<int> { -1, 0, 1, 2, 3 } },
+            new object[] { new List<int> { 1, 1, 1 }, new List<int> { 1, 1, 1 } }
+        };
+
+        [TestCaseSource(nameof(Cases))]
+        public void BubbleSortTest(List<int> listToSort, List<int> expectedList)
+        {
+            Program.BubbleSort(listToSort, comparer);
+            Assert.AreEqual(expectedList, listToSort);
+        }
+
+        [Test()]
+        public void BubbleSortListOfStringsTest()
+        {
+            var listToSort = new List<string> { "sd", "bc", "abd" };
+            var expectedList = new List<string> { "abd", "bc", "sd" };
+            var stringComarer = Comparer<string>.Default;
+
+            Program.BubbleSort(listToSort, stringComarer);
+
+            Assert.AreEqual(expectedList, listToSort);
+        }
+    }
+}
