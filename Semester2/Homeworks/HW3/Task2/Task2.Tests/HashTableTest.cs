feccd2c2e9b8f959f8d4c9d0fece92fd4fa1bd6e
Pavel <pavsaltykov@mail.ru>
2020-06-08 19:06:29 +0300

    added list test

---
+++ Semester2/Homeworks/HW3/Task2/Task2.Tests/HashTableTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Task2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2.Tests
+{
+    [TestFixture()]
+    public class HashTableTest
+    {
+        [Test()]
+        public void AddValueTest()
+        {
+
+        }
+
+        [Test()]
+        public void DeleteValueTest()
+        {
+
+        }
+
+        [Test()]
+        public void ContainsTest()
+        {
+
+        }
+
+        [Test()]
+        public void ChangeHashFunctionTest()
+        {
+
+        }
+
+        [Test()]
+        public void ClearTest()
+        {
+
+        }
+    }
+}
---
+++ Semester2/Homeworks/HW3/Task2/Task2.Tests/ListTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Task2.Tests
+{
+    [TestFixture()]
+    public class ListTest
+    {
+        private List list;
+
+        [SetUp()]
+        public void Initialize()
+        {
+            list = new List();
+        }
+
+        [Test()]
+        public void AddToListTest()
+        {
+            Assert.IsTrue(list.IsEmpty);
+            list.AddToList("a");
+            Assert.IsFalse(list.IsEmpty);
+        }
+
+        [Test()]
+        public void DeleteTest()
+        {
+            list.AddToList("ab");
+
+            Assert.IsFalse(list.Delete("ba"));
+            Assert.IsTrue(list.Delete("ab"));
+            Assert.IsTrue(list.IsEmpty);
+        }
+
+        [Test()]
+        public void ContainsTest()
+        {
+            list.AddToList("abc");
+            Assert.IsFalse(list.Contains("abcd"));
+            Assert.IsTrue(list.Contains("abc"));
+        }
+
+        [Test()]
+        public void DeleteFromHeadOfEmptyListTest()
+        {
+            Assert.Throws(typeof(InvalidOperationException), () => list.DeleteFromHead());
+        }
+
+        [Test()]
+        public void DeleteFromHeadTest()
+        {
+            list.AddToList("abc");
+            Assert.AreEqual("abc", list.DeleteFromHead());
+            Assert.IsTrue(list.IsEmpty);
+        }
+    }
+}
